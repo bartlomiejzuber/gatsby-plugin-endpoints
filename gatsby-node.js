debc904c1ea378a4370af24cdc3396a96f49aa29
Bart Zuber <bzuber@neworbit.co.uk>
2022-08-22 19:59:37 +0200

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,38 @@
+let graphql;
+
+// Generate template data and inject into pages
+exports.createPages = ({ graphql: graphqlObj }) => {
+  graphql = graphqlObj;
+};
+
+exports.onCreateDevServer = async ({ app }) => {
+    app.get("/endpoints.json", async (req, res) => {
+      let {
+        data: {
+          pages: { edges: allPagesData },
+        },
+      } = await graphql(
+        `
+          {
+            pages: allSitePage {
+              edges {
+                node {
+                  path
+                  pageContext
+                }
+              }
+            }
+          }
+        `
+      );
+  
+      res.setHeader("Content-Type", "application/json");
+      res.send(
+        JSON.stringify(
+          allPagesData.map(({ node: { path } }) => path),
+          undefined,
+          3
+        )
+      );
+    });
+  };
