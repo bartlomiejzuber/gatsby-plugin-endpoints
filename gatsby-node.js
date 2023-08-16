8dec2571be9d8a314d191d8cb7b19342a9b83388
Bart Zuber <bzuber@neworbit.co.uk>
2023-08-16 12:30:18 +0200

    chore: add onPostBootstrap

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,38 +5,69 @@ exports.createPages = ({ graphql: graphqlObj }) => {
   graphql = graphqlObj;
 };
 
+exports.onPostBootstrap = async () => {
+  let {
+    data: {
+      pages: { edges: allPagesData },
+    },
+  } = await graphql(
+    `
+      {
+        pages: allSitePage {
+          edges {
+            node {
+              path
+              pageContext
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const endpoints = allPagesData.map(({ node }) => node.path);
+
+  require("fs/promises").writeFile(
+    "./public/endpoints.json",
+    JSON.stringify(endpoints),
+    { encoding: "utf-8" }
+  );
+};
+
 exports.onCreateDevServer = async ({ app }, pluginOptions = {}) => {
-    app.get("/endpoints.json", async (req, res) => {
-      let {
-        data: {
-          pages: { edges: allPagesData },
-        },
-      } = await graphql(
-        `
-          {
-            pages: allSitePage {
-              edges {
-                node {
-                  path
-                  pageContext
-                }
+  app.get("/endpoints.json", async (req, res) => {
+    let {
+      data: {
+        pages: { edges: allPagesData },
+      },
+    } = await graphql(
+      `
+        {
+          pages: allSitePage {
+            edges {
+              node {
+                path
+                pageContext
               }
             }
           }
-        `
-      );
+        }
+      `
+    );
 
-      if (pluginOptions.filter) {
-        allPagesData = allPagesData.filter((item) => pluginOptions.filter(item, req));
-      }
-  
-      res.setHeader("Content-Type", "application/json");
-      res.send(
-        JSON.stringify(
-          allPagesData.map(({ node: { path } }) => path),
-          undefined,
-          3
-        )
+    if (pluginOptions.filter) {
+      allPagesData = allPagesData.filter((item) =>
+        pluginOptions.filter(item, req)
       );
-    });
-  };
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.send(
+      JSON.stringify(
+        allPagesData.map(({ node: { path } }) => path),
+        undefined,
+        3
+      )
+    );
+  });
+};
